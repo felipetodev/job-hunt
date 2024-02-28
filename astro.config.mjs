b49ea5f1049f4f2a358ba7c9ef2fa391f406cf03
felipetodev <fe.ossandon.u@gmail.com>
2024-02-28 17:01:11 -0300

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
   })],
-  output: "server"
+  output: "server",
+  adapter: vercel()
 });
