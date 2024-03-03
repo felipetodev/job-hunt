866cf45e9c5164f8ffb3aa105ec165b2e5efca80
felipetodev <fe.ossandon.u@gmail.com>
2024-03-03 00:02:32 -0300

    fix: refact filter code

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,4 @@
-import type { Job } from "@/types";
-
-const svg = `
+export const currencyOffIcon = `
   <svg
     xmlns="http://www.w3.org/2000/svg"
     class="w-4 h-4"
@@ -17,28 +15,3 @@ const svg = `
     ></path><path d="M12 3v3m0 12v3"></path><path d="M3 3l18 18"></path></svg
   >
 `.trim();
-
-export const generateTableRow = (job: Job) => {
-  return `
-    <tr data-href=${job.url} class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted group cursor-pointer">
-      <td class="relative p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 group-hover:underline underline-offset-2">
-        ${job.title}
-      </td>
-      <td class="relative p-4 align-middle [&:has([role=checkbox])]:pr-0">
-        ${job.company}
-      </td>
-      <td class="relative p-4 align-middle [&:has([role=checkbox])]:pr-0 max-w-[30ch] truncate">
-        ${job.location}
-      </td>
-      <td class="relative p-4 align-middle [&:has([role=checkbox])]:pr-0">
-        ${job.modality}
-      </td>
-      <td class="relative p-4 align-middle [&:has([role=checkbox])]:pr-0">
-        ${job.payment ?? `<span class="flex items-center gap-x-2">${svg} N/A</span>`}
-      </td>
-      <td class="relative p-4 align-middle [&:has([role=checkbox])]:pr-0">
-        ${job.published}, ${job.year}
-      </td>
-    </tr>
-  `.trim();
-};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,34 @@
+import { currencyOffIcon } from "@/lib/constants";
+import type { Job } from "@/types";
+
 export const centerRectOnScreen = (targetWidth: number, targetHeight: number) => {
   const left = (window.innerWidth - targetWidth) / 2 + window.screenLeft
   const top = (window.innerHeight - targetHeight) / 2 + window.screenTop
   return { width: targetWidth, height: targetHeight, left, top }
 }
+
+export const generateTableRow = (job: Job) => {
+  return `
+    <tr data-href=${job.url} class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted group cursor-pointer">
+      <td class="relative p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 group-hover:underline underline-offset-2">
+        ${job.title}
+      </td>
+      <td class="relative p-4 align-middle [&:has([role=checkbox])]:pr-0">
+        ${job.company}
+      </td>
+      <td class="relative p-4 align-middle [&:has([role=checkbox])]:pr-0 max-w-[30ch] truncate">
+        ${job.location}
+      </td>
+      <td class="relative p-4 align-middle [&:has([role=checkbox])]:pr-0">
+        ${job.modality}
+      </td>
+      <td class="relative p-4 align-middle [&:has([role=checkbox])]:pr-0">
+        ${job.payment ?? `<span class="flex items-center gap-x-2">${currencyOffIcon} N/A</span>`}
+      </td>
+      <td class="relative p-4 align-middle [&:has([role=checkbox])]:pr-0">
+        ${job.published}, ${job.year}
+      </td>
+    </tr>
+  `.trim();
+};
+
