626b5611b4ceb520d8c1fd0d4ed6eb4c540398b7
felipetodev <fe.ossandon.u@gmail.com>
2024-03-05 10:24:07 -0300

    feat: scrape more jobs categories and add last updated timestamp

--- src/script/index.ts
+++ src/script/index.ts
@@ -3,24 +3,46 @@ import { writeFile } from 'node:fs/promises'
 import path from 'node:path'
 import type { Job } from '@/types'
 
-const MAIN_JOB_CARD = '.gb-results-list__item'
+export const MAIN_LIST_SELECTORS = {
+  MAIN_JOB_CARD: '.gb-results-list__item',
+  JOB_TITLE: '.gb-results-list__title strong',
+  JOB_COMPANY: '.gb-results-list__info div strong',
+  JOB_LOCATION: '.gb-results-list__info .location',
+  JOB_PAYMENT_RANGE: '.fa.fa-money.tooltipster-basic.green',
+  JOB_PUBLISHED_DATE: '.opacity-half.size0'
+} as const
 
-const JOB_TITLE = '.gb-results-list__title strong'
-const JOB_COMPANY = '.gb-results-list__info div strong'
-const JOB_LOCATION = '.gb-results-list__info .location'
-const JOB_PAYMENT_RANGE = '.fa.fa-money.tooltipster-basic.green'
-const JOB_PUBLISHED_DATE = '.opacity-half.size0'
+export const JOBS_CATEGORY = [
+  'programacion',
+  'sysadmin-devops-qa',
+  'cybersecurity',
+  'desarrollo-mobile'
+] as const
+
+export const months = [
+  "ene",
+  "feb",
+  "mar",
+  "abr",
+  "may",
+  "jun",
+  "jul",
+  "ago",
+  "sep",
+  "oct",
+  "nov",
+  "dic"
+];
 
 const dateComparison = (date: string) => {
   const actualDate = new Date()
   const actualMonth = actualDate.getMonth()
   const actualDay = actualDate.getDate()
 
-  const { 0: day, 1: month } = date.split(" ")
+  const [day, month] = date.split(" ")
   const monthDate = day.toLowerCase()
   const dayDate = Number(month)
 
-  const months = ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"]
   const monthIndex = months.indexOf(monthDate)
 
   if ((actualMonth > monthIndex) || (actualMonth === monthIndex && actualDay >= dayDate)) {
@@ -31,48 +53,22 @@ const dateComparison = (date: string) => {
 
 }
 
-const scrape = async (job = 'programacion') => {
-  const web = await fetch(`https://www.getonbrd.com/empleos/${job}?lang=es`)
+const scrape = async (category: typeof JOBS_CATEGORY[number]) => {
+  const web = await fetch(`https://www.getonbrd.com/empleos/${category}?lang=es`)
   const html = await web.text()
   return cheerio.load(html)
 }
 
-// const getMoreJobInfo = async (jobs: any) => {
-//   const jobsData: any = []
-
-//   for (const job of jobs) {
-//     const web = await fetch(job.url)
-//     const html = await web.text()
-//     const $ = cheerio.load(html)
-
-//     $('.gb-landing-cover').each((i, el) => {
-//       const time = $(el).find('time').text().trim()
-//       const dateString = time.replaceAll(' de ', ' ')
-//       const date = new Date(dateString)
-
-//       const jobData = {
-//         ...job,
-//         published: time,
-//         publish_timestamp: date.getTime()
-//       }
-
-//       jobsData.push(jobData)
-//     })
-//   }
-
-//   return jobsData
-// }
-
-const getListOfJobs = async () => {
-  const $ = await scrape()
+const getListOfJobs = async (category: typeof JOBS_CATEGORY[number]) => {
+  const $ = await scrape(category)
   const jobs: Job[] = []
 
-  $(MAIN_JOB_CARD).each((i, el) => {
-    const title = $(el).find(JOB_TITLE).text()
-    const company = $(el).find(JOB_COMPANY).text()
-    const location = $(el).find(JOB_LOCATION).text().replaceAll('\n', '').trim()
-    const payment = $(el).find(JOB_PAYMENT_RANGE).attr('title') || null
-    const published = $(el).find(JOB_PUBLISHED_DATE).text().trim()
+  $(MAIN_LIST_SELECTORS.MAIN_JOB_CARD).each((i, el) => {
+    const title = $(el).find(MAIN_LIST_SELECTORS.JOB_TITLE).text()
+    const company = $(el).find(MAIN_LIST_SELECTORS.JOB_COMPANY).text()
+    const location = $(el).find(MAIN_LIST_SELECTORS.JOB_LOCATION).text().replaceAll('\n', '').trim()
+    const payment = $(el).find(MAIN_LIST_SELECTORS.JOB_PAYMENT_RANGE).attr('title') || null
+    const published = $(el).find(MAIN_LIST_SELECTORS.JOB_PUBLISHED_DATE).text().trim()
     const url = $(el).attr('href')!
 
     const isHybrid = "El trabajo se desempeña algunos días de forma remota y otros en la oficina"
@@ -100,35 +96,23 @@ const getListOfJobs = async () => {
   })
 
   console.info(jobs.length + ' jobs generated ✨✨')
+  return jobs.filter(job => job.year >= new Date().getFullYear())
+}
 
+function sortJobsByDate(jobs: Job[]) {
   return jobs.sort((a, b) => {
-    const { 0: dayA, 1: monthA } = a.published.split(" ")
-    const { 0: dayB, 1: monthB } = b.published.split(" ")
-    
+    const [dayA, monthA] = a.published.split(" ")
+    const [dayB, monthB] = b.published.split(" ")
+
     const monthDateA = dayA.toLowerCase()
     const monthDateB = dayB.toLowerCase()
-  
+
     const dayDateA = Number(monthA);
     const dayDateB = Number(monthB);
-  
-    const months = [
-      "ene",
-      "feb",
-      "mar",
-      "abr",
-      "may",
-      "jun",
-      "jul",
-      "ago",
-      "sep",
-      "oct",
-      "nov",
-      "dic",
-    ]
-  
+
     const monthIndexA = months.indexOf(monthDateA)
     const monthIndexB = months.indexOf(monthDateB)
-  
+
     return (
       new Date(b.year, monthIndexB, dayDateB).getTime() -
       new Date(a.year, monthIndexA, dayDateA).getTime()
@@ -136,12 +120,34 @@ const getListOfJobs = async () => {
   });
 }
 
-const writeDBFile = async (data: Job[]) => {
-  const filePath = path.join(process.cwd(), './db/gob-jobs.json')
-  return await writeFile(filePath, JSON.stringify(data, null, 2), 'utf-8')
+const writeDBFile = (data: Job[]) => {
+  const pathArr = [
+    {
+      path: './db/gob-jobs.json',
+      data
+    },
+    {
+      path: './db/info.json',
+      data: { lastModified: +new Date() }
+    }
+  ]
+
+  pathArr.forEach(async ({ path: _path, data }) => {
+    const filePath = path.join(process.cwd(), _path)
+    await writeFile(filePath, JSON.stringify(data, null, 2), 'utf-8')
+  })
 }
 
 (async () => {
-  const jobsData = await getListOfJobs()
-  writeDBFile(jobsData)
+  const allJobs: Job[] = []
+  for (const category of JOBS_CATEGORY) {
+    const jobs = await getListOfJobs(category)
+    allJobs.push(...jobs)
+
+    console.info(`✅ ${category} jobs done`)
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+  }
+  const sortedJobs = sortJobsByDate(allJobs)
+  writeDBFile(sortedJobs)
+  console.info('DB updated successfully 🚀🚀')
 })()
